7a9dd40f3bd4c4eb4ac8b21488ab9d40d0811e01
Jesus Ramirez Velasco <jesusmarcos81@gmail.com>
2015-01-29 11:38:43 -0600

    Agregamos funcion get_available_seats

--- openacademy.py
+++ openacademy.py
@@ -14,6 +14,26 @@ class course (osv.Model):
 class session (osv.Model):
     _name = 'openacademy.session'
     
+    def get_available_seats(self, cr, uid, ids, field, arg, context={}):
+        res = {}
+        sessions = self.browse(cr, uid, ids, context=context)
+        for session in sessions:
+            #Con try
+            #try:
+            #    res[session.id] = len(session.attendee_ids) / session.seats * 100
+            #except ZeroDivisionError:
+            #    res[session.id] = 0.0
+            
+            #Con if
+            #if session.seats == 0.0:
+            #    res[session.id] = 0.0
+            #else:
+            #    res[session.id] = len(session.attendee_ids) / session.seats * 100
+            
+            #Con python
+            res[session.id] = session.seats == 0.0 and 0.0 or len(session.attendee_ids) / session.seats * 100
+        return res
+    
     _columns = {
         'name':         fields.char(string="Name", size=128, required=True),
         'start_date':   fields.date(string="Start date"),
@@ -23,6 +43,7 @@ class session (osv.Model):
                                         domain="['|',('instructor','=',True),('category_id.name','in',['Teacher level 1', 'Teacher level 2'])]"),
         'course_id':    fields.many2one('openacademy.course', string="Course", ondelete="cascade"),
         'attendee_ids': fields.one2many('openacademy.attendee', 'session_id', string="Attendees"),
+        'available_seats':  fields.function(get_available_seats, type="float", string="Available Seats", readonly=True)
     }
 
 class attendee (osv.Model):
