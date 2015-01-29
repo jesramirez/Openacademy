c55b3040bf05feec5d24f8cba9d7393314fc36cf
Jesus Ramirez Velasco <jesusmarcos81@gmail.com>
2015-01-29 09:02:27 -0600

    Se agrega mas modulos session, attendee y los menus para accesar a ellos

--- openacademy.py
+++ openacademy.py
@@ -5,6 +5,31 @@ class course (osv.Model):
     _name = 'openacademy.course'
     
     _columns = {
+        'name':             fields.char(string="Name", size=128, required=True),
+        'description':      fields.text(string="Description"),
+        'responsible_id':   fields.many2one('res.users', string="Responsible", required=True),
+    }
+
+class session (osv.Model):
+    _name = 'openacademy.session'
+    
+    _columns = {
         'name':         fields.char(string="Name", size=128, required=True),
-        'description':  fields.text(string="Description"),
+        'start_date':   fields.date(string="Start date"),
+        'duration':     fields.float(string="Duration", digits=(6,2), help="Session durantion in days"),
+        'seats':        fields.integer(string="Number of seats"),
+        'instructor_id':fields.many2one('res.partner', string="Instructor", ondelete="set null"),
+        'course_id':    fields.many2one('openacademy.course', string="Course", ondelete="cascade"),
+    }
+
+class attendee (osv.Model):
+    _name = 'openacademy.attendee'
+    
+    _rec_name = 'partner_id'
+    _order = 'partner_id'
+    
+    _columns = {
+        'name':         fields.char(string="Name", size=256),
+        'partner_id':   fields.many2one('res.partner', string="Partner"),
+        'session_id':   fields.many2one('openacademy.session', string="Attended session", ondelete="cascade"),
     }
