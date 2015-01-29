24440f05d4a76f57013c96f72bf9319d81a56c07
Jesus Ramirez Velasco <jesusmarcos81@gmail.com>
2015-01-29 08:37:55 -0600

    Creacion de init, openacademy

--- __openerp__.py
+++ __openerp__.py
@@ -1,1 +1,27 @@
-1
+
+{
+    "name": "OpenAcademy",
+    "version": "1.0",
+    "depends": [
+        "base",
+    ],
+    "author": "Carlos Contreras",
+    "category": "Academic",
+    "description": """
+    OpenAcademy
+    ===========
+    
+    Este módulo permite la gestión de centros educativos.
+    """,
+    'data': [
+        #all other data files, except demo data and tests
+        ],
+    'demo': [
+        #files containg demo data
+    ],
+    'test': [
+        #files containg tests
+    ],
+    'installable': True,
+    'auto_install': False,
+}
